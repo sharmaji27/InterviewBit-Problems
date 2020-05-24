3c8464a1b0e5fdb225671ae9c96c851a9d5e4d06
sharmaji <asharma70420@gmail.com>
2020-05-24 11:47:51 +0530

    Strings

---
+++ Strings/Amazing Subarrays.py
@@ -0,0 +1,44 @@
+'''
+You are given a string S, and you have to find all the amazing substrings of S.
+
+Amazing Substring is one that starts with a vowel (a, e, i, o, u, A, E, I, O, U).
+
+Input
+
+Only argument given is string S.
+Output
+
+Return a single integer X mod 10003, here X is number of Amazing Substrings in given string.
+Constraints
+
+1 <= length(S) <= 1e6
+S can have special characters
+Example
+
+Input
+    ABEC
+
+Output
+    6
+
+Explanation
+	Amazing substrings of given string are :
+	1. A
+	2. AB
+	3. ABE
+	4. ABEC
+	5. E
+	6. EC
+	here number of substrings are 6 and 6 % 10003 = 6.
+'''
+class Solution:
+    # @param A : string
+    # @return an integer
+    def solve(self, A):
+        vowels = 'aeiouAEIOU'
+        c=0
+        n=len(A)
+        for i in range(len(A)):
+            if A[i] in vowels:
+                c+=n-i
+        return (c%10003)
