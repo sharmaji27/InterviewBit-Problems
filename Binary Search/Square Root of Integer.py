185c8e13b5394d44d124d5aa0dcaae836bcd3ebc
sharmaji <asharma70420@gmail.com>
2020-05-25 18:03:08 +0530

    Binary Search

---
+++ Binary Search/Square Root of Integer.py
@@ -0,0 +1,48 @@
+'''
+Given an integar A.
+
+Compute and return the square root of A.
+
+If A is not a perfect square, return floor(sqrt(A)).
+
+DO NOT USE SQRT FUNCTION FROM STANDARD LIBRARY
+
+
+
+Input Format
+
+The first and only argument given is the integer A.
+Output Format
+
+Return floor(sqrt(A))
+Constraints
+
+1 <= A <= 10^9
+For Example
+
+Input 1:
+    A = 11
+Output 1:
+    3
+
+Input 2:
+    A = 9
+Output 2:
+    3 
+'''
+class Solution:
+    # @param A : integer
+    # @return an integer
+    def sqrt(self, n):
+        l = 0
+        r = (n//2) +1
+        res = 0
+        
+        while l<=r:
+            m = l + (r-l)//2
+            if m*m<=n:
+                res = m
+                l = m+1
+            else:
+                r=m-1
+        return (res)
