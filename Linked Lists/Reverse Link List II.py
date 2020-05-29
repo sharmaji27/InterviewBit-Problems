5186d4aab6dc1682bd5514f732fc0c378d84adbf
sharmaji <asharma70420@gmail.com>
2020-05-29 19:43:26 +0530

    Linked Lists

---
+++ Linked Lists/Reverse Link List II.py
@@ -0,0 +1,45 @@
+'''
+Reverse a linked list from position m to n. Do it in-place and in one-pass.
+
+For example:
+Given 1->2->3->4->5->NULL, m = 2 and n = 4,
+
+return 1->4->3->2->5->NULL.
+
+ Note:
+Given m, n satisfy the following condition:
+1 ≤ m ≤ n ≤ length of list. Note 2:
+Usually the version often seen in the interviews is reversing the whole linked list which is obviously an easier version of this question. 
+'''
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    # @param A : head node of linked list
+    # @param B : integer
+    # @param C : integer
+    # @return the head node in the linked list
+    def reverseBetween(self, head, m, n):
+        dummyNode = ListNode(0)
+        dummyNode.next = head
+        pre = dummyNode
+
+        for i in range(m - 1):
+            pre = pre.next
+        
+        # reverse the [m, n] nodes
+        reverse = None
+        cur = pre.next
+        for i in range(n - m + 1):
+            next = cur.next
+            cur.next = reverse
+            reverse = cur
+            cur = next
+
+        pre.next.next = cur
+        pre.next = reverse
+
+        return dummyNode.next       
