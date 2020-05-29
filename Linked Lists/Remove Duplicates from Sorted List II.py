049e94c7bffb7b8c028ec6dc9eee8d5f3cf58109
sharmaji <asharma70420@gmail.com>
2020-05-29 11:25:54 +0530

    Linked Lists

---
+++ Linked Lists/Remove Duplicates from Sorted List II.py
@@ -0,0 +1,33 @@
+'''
+Given a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list.
+
+For example,
+Given 1->2->3->3->4->4->5, return 1->2->5.
+Given 1->1->1->2->3, return 2->3.
+'''
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    # @param A : head node of linked list
+    # @return the head node in the linked list
+    def deleteDuplicates(self, head):
+        dummy = ListNode(0);  # construct a dummy node
+        dummy.next = head 
+
+        pre = dummy           # set up pre and cur pointers
+        cur = head
+        while cur:
+            if cur.next and cur.val == cur.next.val:
+                # loop until cur point to the last duplicates
+                while cur and cur.next and cur.val == cur.next.val:
+                    cur = cur.next
+                pre.next = cur.next  # propose the next for pre
+                                     # this will be verified by next line
+            else:
+                pre = pre.next 
+            cur = cur.next
+        return dummy.next
