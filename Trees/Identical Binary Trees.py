738b0ff5ff9f6e85a57fa3589d8db981d9e301f0
sharmaji <asharma70420@gmail.com>
2020-06-04 16:26:02 +0530

    Trees

---
+++ Trees/Identical Binary Trees.py
@@ -0,0 +1,31 @@
+'''
+Given two binary trees, write a function to check if they are equal or not.
+
+Two binary trees are considered equal if they are structurally identical and the nodes have the same value.
+
+Return 0 / 1 ( 0 for false, 1 for true ) for this problem
+
+Example :
+
+Input : 
+
+   1       1
+  / \     / \
+ 2   3   2   3
+
+Output : 
+  1 or True
+'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        if not p and not q:return 1
+        if not p or not q:return 0
+        if p.val!=q.val:return 0
+        return self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
