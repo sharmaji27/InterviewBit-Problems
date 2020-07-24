6dbbb06525b1bbbcd666e2a97f7d349267b1ae08
sharmaji <asharma70420@gmail.com>
2020-07-24 19:44:12 +0530

    Greedy

---
+++ Greedy/Highest Product.py
@@ -0,0 +1,47 @@
+'''
+Given an array A, of N integers A.
+
+Return the highest product possible by multiplying 3 numbers from the array.
+
+NOTE: Solution will fit in a 32-bit signed integer.
+
+
+
+Input Format:
+
+The first and the only argument is an integer array A.
+
+Output Format:
+
+Return the highest possible product.
+
+Constraints:
+
+1 <= N <= 5e5
+
+Example:
+
+Input 1:
+A = [1, 2, 3, 4]
+
+Output 1:
+24
+
+Explanation 1:
+2 * 3 * 4 = 24
+
+Input 2:
+A = [0, -1, 3, 100, 70, 50]
+
+Output 2:
+350000
+
+Explanation 2:
+70 * 50 * 100 = 350000
+'''
+class Solution:
+	# @param A : list of integers
+	# @return an integer
+	def maxp3(self, A):
+        A.sort()
+        return max(A[-3]*A[-2]*A[-1],A[0]*A[1]*A[2],A[0]*A[1]*A[-1])
