f9e0f1bccb8d5a17cc5aa4717c486fb3d7fc07f8
sharmaji27 <asharma70420@gmail.com>
2020-08-01 14:23:27 +0530

    Trees

---
+++ Trees/Sorted Array To Balanced BST.py
@@ -0,0 +1,31 @@
+'''
+Given an array where elements are sorted in ascending order, convert it to a height balanced BST.
+
+ Balanced tree : a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1. 
+Example :
+
+
+Given A : [1, 2, 3]
+A height balanced BST  : 
+
+      2
+    /   \
+   1     3
+'''
+# Definition for a  binary tree node
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    # @param A : tuple of integers
+    # @return the root node in the tree
+    def sortedArrayToBST(self, A):
+        if len(A)==0:return
+        mid = len(A)//2
+        root = TreeNode(A[mid])
+        root.left = self.sortedArrayToBST(A[:mid])
+        root.right = self.sortedArrayToBST(A[mid+1:])
+        return root
