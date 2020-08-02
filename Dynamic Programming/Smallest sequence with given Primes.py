9835706d99f470ecabea4b273809cedf2d71ddaf
sharmaji27 <asharma70420@gmail.com>
2020-08-02 17:33:05 +0530

    DP

---
+++ Dynamic Programming/Smallest sequence with given Primes.py
@@ -0,0 +1,61 @@
+'''
+GIven three prime numbers A, B and C and an integer D.
+
+You need to find the first(smallest) D integers which only have A, B, C or a combination of them as their prime factors.
+
+
+
+Input Format
+First argument is an integer A.
+
+Second argument is an integer B.
+
+Third argument is an integer C.
+
+Fourth argument is an integer D.
+
+
+
+Output Format
+Return an array of size D denoting the first smallest D integers which only have A, B, C or a combination of them as their prime factors.
+
+NOTE: You need to return the array sorted in ascending order.
+
+
+
+Example Input
+Input 1:
+
+ A = 2
+ B = 3
+ C = 5
+ D = 5
+
+
+Example Output
+Output 1:
+
+ [2, 3, 4, 5, 6]
+
+
+Example Explanation
+Explanation 1:
+
+ 4 = A * A i.e ( 2 * 2 ) 
+ 6 = A * B i.e ( 2 * 3 )
+'''
+class Solution:
+    # @param A : integer
+    # @param B : integer
+    # @param C : integer
+    # @param D : integer
+    # @return a list of integers
+    def solve(self, A, B, C, D):
+        d = {A:1,B:1,C:1}
+        ans = []
+        for i in range(D):
+            curr = min(d)
+            del d[curr]
+            ans.append(curr)
+            d[A*curr]=d[B*curr]=d[C*curr]=1
+        return ans
