75a8a3ad455cd53a5b830e3a70109d292f12c720
sharmaji27 <asharma70420@gmail.com>
2020-08-22 19:26:59 +0530

    Graph

--- Graph/Word Ladder I.py
+++ Graph/Word Ladder I.py
@@ -1,3 +1,40 @@
+'''
+Given two words A and B, and a dictionary, C, find the length of shortest transformation sequence from A to B, such that:
+
+You must change exactly one character in every transformation.
+Each intermediate word must exist in the dictionary.
+Note:
+
+Return 0 if there is no such transformation sequence.
+All words have the same length.
+All words contain only lowercase alphabetic characters.
+
+
+Input Format:
+
+The first argument of input contains a string, A.
+The second argument of input contains a string, B.
+The third argument of input contains an array of strings, C.
+Output Format:
+
+Return an integer representing the minimum number of steps required to change string A to string B.
+Constraints:
+
+1 <= length(A), length(B), length(C[i]) <= 25
+1 <= length(C) <= 5e3
+Example :
+
+Input 1:
+    A = "hit"
+    B = "cog"
+    C = ["hot", "dot", "dog", "lot", "log"]
+
+Output 1:
+    5
+
+Explanation 1:
+    "hit" -> "hot" -> "dot" -> "dog" -> "cog"
+'''
 class Solution:
     # @param A : string
     # @param B : string
