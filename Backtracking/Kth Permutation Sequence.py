a21c9d56fe0238c48ef5ffd3ecdf870f501e1602
sharmaji27 <asharma70420@gmail.com>
2020-09-02 13:01:44 +0530

    Backtracking

---
+++ Backtracking/Kth Permutation Sequence.py
@@ -0,0 +1,40 @@
+'''
+The set [1,2,3,…,n] contains a total of n! unique permutations.
+
+By listing and labeling all of the permutations in order,
+We get the following sequence (ie, for n = 3 ) :
+
+1. "123"
+2. "132"
+3. "213"
+4. "231"
+5. "312"
+6. "321"
+Given n and k, return the kth permutation sequence.
+
+For example, given n = 3, k = 4, ans = "231"
+
+ Good questions to ask the interviewer :
+What if n is greater than 10. How should multiple digit numbers be represented in string?
+ In this case, just concatenate the number to the answer.
+so if n = 11, k = 1, ans = "1234567891011" 
+Whats the maximum value of n and k?
+ In this case, k will be a positive integer thats less than INT_MAX.
+n is reasonable enough to make sure the answer does not bloat up a lot.
+'''
+class Solution:
+    # @param A : integer
+    # @param B : integer
+    # @return a strings
+    def getPermutation(self, n, k):
+        numbers = list(range(1, n+1))
+        permutation = ''
+        k -= 1
+        while n > 0:
+            n -= 1
+            # get the index of current digit
+            index, k = divmod(k, math.factorial(n))
+            permutation += str(numbers[index])
+            # remove handled number
+            numbers.remove(numbers[index])
+        return permutation
